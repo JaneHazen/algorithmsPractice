30fe5c40e4046ded8088bcde7803f618ec316cc5
janehazen <iamjane@gmail.com>
2018-07-07 16:54:58 -0700

    FEM make change

--- frontendmastersalgos.js
+++ frontendmastersalgos.js
@@ -218,3 +218,22 @@ function merge(left, right) {
 }
 
 mergeSort([1, 3, 6, 8, 12, 14])
+
+// MAKE CHANGE
+
+
+function makeChange(coins, amount){
+    coins.sort((a, b) => b -a);
+    let coinTotal = 0;
+    let i = 0
+    while (amount > 0 ){
+        if(coins[i] <= amount){
+            amount -= coins[i];
+            coinTotal++;
+        } else {
+            i++;
+        }
+    }
+    return coinTotal
+}
+makeChange([5, 10, 25], 50);
