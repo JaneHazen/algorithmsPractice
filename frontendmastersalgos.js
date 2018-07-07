d9900ceeb9fafba347edbe305852f5d7105692ac
janehazen <iamjane@gmail.com>
2018-07-07 14:26:05 -0700

    FEM recursive factorial memoization

--- frontendmastersalgos.js
+++ frontendmastersalgos.js
@@ -110,3 +110,28 @@ const recursiveFactorial = (n, result=1) => {
 
 const recursive = recursiveFactorial(5)
 console.log(recursive)
+
+
+const memoize = (fn) => {
+    let cache = {};
+    return(...args) => {
+        let n = args[0];
+        if(n in cache){
+            return cache[n];
+        } else {
+            let result = fn(n);
+            cache[n] = result;
+            return result;
+        }
+    }
+}
+const factorial = memoize(
+    (x) => {
+        if(x === 0){
+            return 1;
+        } else {
+            return x * factorial(x -1)
+        }
+    })
+
+console.log(factorial(5))
